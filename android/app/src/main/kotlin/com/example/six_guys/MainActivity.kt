5aed47ffd7795571cda66dea98141dd28af06929
hi-jin <crushed7@o.cnu.ac.kr>
2024-01-24 11:10:40 -0800

    Build: fix build settings

--- android/app/src/main/kotlin/com/example/six_guys/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/six_guys/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.six_guys
+package com.irvine.six_guys
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:six_guys/core/app_router.dart';
+import 'package:six_guys/firebase_options.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const ProviderScope(child: MyApp()));
 }
